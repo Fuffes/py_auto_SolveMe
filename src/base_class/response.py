391ed7f52c96e536395e1763454d5ccf28556e1d
Fuffes <Fuffes05@gmail.com>
2024-05-13 17:41:04 +0300

    #3.3

--- src/base_class/response.py
+++ src/base_class/response.py
@@ -2,10 +2,11 @@ from ..enums.global_enums import GlobalErrorMessages as error
 
 
 class Response():
-
+    # TODO check problem with   self.response_data = self.response_json.get("data")
     def __init__(self, resp):
         self.response = resp
         self.response_json = resp.json()
+        # self.response_data = self.response_json.get("data")
         self.response_status_code = resp.status_code
 
     #shema
@@ -15,18 +16,26 @@ class Response():
     #             validate(item, schema)
     #     else: validate(self.response_json, schema)
 
+    # TODO modify validate method to work with "data"
     #PYDANTIC
     def validate(self, schema):
-        if isinstance(self.response_json, list):
-            for item in self.response_json:
+        if isinstance(self.response_json.get("data"), list):
+            for item in self.response_json.get("data"):
                 schema(**item)
-        else: schema(**self.response_json)
+        else: schema(**self.response_json.get("data"))
 
 
     def assert_status_code(self, exp_status_code):
         if isinstance(exp_status_code, list):
             for status in exp_status_code:
-                assert self.response_status_code == status, error.WRONG_STATUS_CODE.value
+                assert self.response_status_code == status, self
         else:
-            assert self.response_status_code == exp_status_code, error.WRONG_STATUS_CODE.value
+            assert self.response_status_code == exp_status_code, self
         return self
+
+
+    def __str__(self):
+        return (
+                f"status code: {self.response_status_code} \n\
+                req. url : {self.response.url}\n\ resp.body: {self.response_json}"
+                )
---
+++ tests/test_gorest.py
@@ -0,0 +1,30 @@
+import requests
+from src.base_class.response import Response
+from configuration import BASE_URL
+from src.pydantic_schemas.user import User
+
+
+def test_get_data():
+    resp = requests.get(BASE_URL)
+    data = Response(resp)
+    data.assert_status_code(300).validate(User)
+
+
+# {'meta':
+#      {'pagination':
+#           {'total': 2735,
+#            'pages': 274,
+#            'page': 1,
+#            'limit': 10,
+#            'links': {'previous': None, 'current': 'https://gorest.co.in/public/v1/users?page=1', 'next': 'https://gorest.co.in/public/v1/users?page=2'}}},
+#  'data': [
+#      {'id': 6888703, 'name': 'Charuchandra Ahuja', 'email': 'ahuja_charuchandra@ortiz.example', 'gender': 'female', 'status': 'active'},
+#      {'id': 6888702, 'name': 'Akroor Guha LLD', 'email': 'lld_akroor_guha@dooley.example', 'gender': 'male', 'status': 'inactive'},
+#      {'id': 6888701, 'name': 'The Hon. Eshita Desai', 'email': 'the_desai_hon_eshita@collier.test', 'gender': 'male', 'status': 'active'},
+#      {'id': 6888700, 'name': 'Sloka Bandopadhyay', 'email': 'bandopadhyay_sloka@bogan-streich.example', 'gender': 'male', 'status': 'active'},
+#      {'id': 6888698, 'name': 'Param Tandon', 'email': 'param_tandon@ward.test', 'gender': 'female', 'status': 'active'},
+#      {'id': 6888697, 'name': 'Geeta Tagore MD', 'email': 'tagore_md_geeta@brekke.example', 'gender': 'male', 'status': 'active'},
+#      {'id': 6888696, 'name': 'Amrita Mukhopadhyay', 'email': 'mukhopadhyay_amrita@lehner.test', 'gender': 'female', 'status': 'inactive'},
+#      {'id': 6888695, 'name': 'Vidhur Patel', 'email': 'patel_vidhur@hettinger.test', 'gender': 'male', 'status': 'inactive'},
+#      {'id': 6888694, 'name': 'Shwet Gupta', 'email': 'gupta_shwet@luettgen.example', 'gender': 'female', 'status': 'inactive'},
+#      {'id': 6888693, 'name': 'Manikya Asan', 'email': 'manikya_asan@boehm.test', 'gender': 'male', 'status': 'inactive'}]}
